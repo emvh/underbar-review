956bbd5f36287876b8eeb412a944399327f656c1
Erfanch <erfan.chinif@gmail.com>
2020-05-12 21:45:41 -0700

    implement delay function

--- src/underbar.js
+++ src/underbar.js
@@ -330,12 +330,14 @@
   };
 
   // Delays a function for the given number of milliseconds, and then calls
-  // it with the arguments supplied.
+  // it with the arguments supplied.``
   //
   // The arguments for the original function are passed after the wait
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+    var args = Array.prototype.slice.call(arguments,2);
+    return setTimeout(function () {return func.apply(this,args)}, wait);
   };
 
 
