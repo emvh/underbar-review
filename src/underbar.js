71647f2e51424eae09d7a33e88cf53e34e908802
Erfanch <erfan.chinif@gmail.com>
2020-05-12 20:55:46 -0700

    implement every function and fix test cases

--- src/underbar.js
+++ src/underbar.js
@@ -195,8 +195,7 @@
       } else {
         accumulator = iterator(accumulator, value);
       }
-
-    });
+    })
     return accumulator;
   };
 
@@ -216,6 +215,10 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    iterator = iterator || _.identity;
+    return !!_.reduce(collection, function(wasFound, value) {
+      return wasFound && iterator(value);
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
